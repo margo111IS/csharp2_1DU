8a57ecc17121c9dc549d4838cf3edd0c22ed353d
margoIS <isachmar@cvut.cz>
2025-04-21 13:35:07 +0200

    Homework_1: added classes Archer and completed Program class.

---
+++ Archery/Archer.cs
@@ -0,0 +1,41 @@
+namespace Archery
+{
+    public class Archer
+    {
+        string _name;
+        int _arrowCount;
+
+        public Archer(string name, int arrowCount)
+        {
+            _name = name;
+            _arrowCount = arrowCount;
+        }
+
+        public void Fire()
+        {
+            if (_arrowCount <= 0)
+            {
+                Console.WriteLine("No arrows available.");
+                return;
+            }
+            _arrowCount--;
+            Console.WriteLine("Arrow has been fired.");
+            return;
+        }
+
+        public void AddArrows(int count)
+        {
+            if (count > 0)
+            {
+                _arrowCount += count;
+                Console.WriteLine($"{count} arrows has been added.");
+            }
+            return;
+        }
+
+        public void ShowState()
+        {
+            Console.WriteLine($"\nSTATUS: {_name} has {_arrowCount} arrows.\n");
+        }
+    }
+}
--- Archery/Program.cs
+++ Archery/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Archery;
+
+public class Program
+{
+    public static void Main()
+    {
+        var archer = new Archer("Archer1", 2);
+        while (true)
+        {
+            archer.ShowState();
+            Console.WriteLine("Choose an option:");
+            try
+            {
+                Console.WriteLine("1. Fire arrow \n2. Add arrows \n3. End");
+                Console.Write("Option: ");
+                string? option = Console.ReadLine();
+                int number = int.Parse(option);
+                if (number != null && number > 0)
+                {
+                    switch (number)
+                    {
+                        case 1:
+                            archer.Fire();
+                            break;
+
+                        case 2:
+                            int result;
+                            while (true)
+                            {
+                                Console.Write("Write a valid number of arrows to add: ");
+                                string count = Console.ReadLine();
+                                result = Program.ReadWholeNumberFromConsole(count);
+                                if (result >= 0)
+                                {
+                                    archer.AddArrows(result);
+                                    break;
+                                }
+                            }
+                            break;
+
+                        case 3:
+                            return;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+    public static int ReadWholeNumberFromConsole(string call)
+    {
+        if (int.TryParse(call, out int input))
+        {
+            if (input >= 0)
+            {
+                return input;
+            }
+            Console.WriteLine("The given number is less than zero! Try again.");
+            return -1;
+        }
+        else
+        {
+            Console.WriteLine("The given number is not valid! Try again.");
+            return -1;
+        }
+    }
+}
